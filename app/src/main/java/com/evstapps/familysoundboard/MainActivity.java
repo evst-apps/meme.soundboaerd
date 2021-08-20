a4e4466b082a3fa790197bccdebd7e4a01c73251
evst-apps <evst.apps@gmail.com>
2021-08-20 18:49:01 +0100

    Paul

--- app/src/main/java/com/evstapps/familysoundboard/MainActivity.java
+++ app/src/main/java/com/evstapps/familysoundboard/MainActivity.java
@@ -10,6 +10,7 @@ import com.google.android.flexbox.FlexboxLayout;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.RequestConfiguration;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.tabs.TabLayout;
@@ -30,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mAdView = findViewById(R.id.adView);
+
         AdRequest adRequest1 = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest1);
 
