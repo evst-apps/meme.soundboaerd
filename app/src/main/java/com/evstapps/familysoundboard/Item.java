eae0886ea3ab31d77e7dfe408613473bf903f8c7
evst-apps <evst.apps@gmail.com>
2021-08-25 13:08:08 +0100

    Paul

--- app/src/main/java/com/evstapps/familysoundboard/Item.java
+++ app/src/main/java/com/evstapps/familysoundboard/Item.java
@@ -5,15 +5,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.Button;
 
+import com.google.android.material.button.MaterialButton;
+
 
 public class Item {
-    public final Button btn;
+    public final MaterialButton btn;
     private final String path;
     @SuppressLint("InflateParams")
     public Item(Context context, String n, String p) {
         String name = n.substring(0, n.lastIndexOf('.'));
         path = p + "/" + n;
-        btn = (Button) LayoutInflater.from(context).inflate(R.layout.button, null);
+        btn = (MaterialButton) LayoutInflater.from(context).inflate(R.layout.button, null);
         btn.setText(name);
         btn.setOnClickListener(view -> Player.Play(context, path));
     }
