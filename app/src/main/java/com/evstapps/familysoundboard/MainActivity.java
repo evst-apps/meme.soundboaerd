4a8a59b4c740bf3b7ff85d74bfebdbf61b7b64fa
evst-apps <evst.apps@gmail.com>
2021-09-01 20:02:35 +0100

    Paul

--- app/src/main/java/com/evstapps/familysoundboard/MainActivity.java
+++ app/src/main/java/com/evstapps/familysoundboard/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MobileAds.initialize(this, initializationStatus -> {
-        });
 
         MobileAds.initialize(this, initializationStatus -> {
             Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
