f993cd13b054688b6ea1bf9555acc46cc6519f3f
evst-apps <evst.apps@gmail.com>
2021-09-18 20:44:15 +0100

    Paul

--- app/src/main/java/com/evstapps/familysoundboard/Item.java
+++ app/src/main/java/com/evstapps/familysoundboard/Item.java
@@ -6,8 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
-
 
 public class Item implements Comparable<Item> {
     public final View view;
@@ -17,7 +15,7 @@ public class Item implements Comparable<Item> {
     public Item(Context context, String n, String p) {
         String name = n.substring(0, n.lastIndexOf('.'));
         path = p + "/" + n;
-        view = (View) LayoutInflater.from(context).inflate(R.layout.activity_button, null);
+        view = LayoutInflater.from(context).inflate(R.layout.activity_button, null);
         textView = view.findViewById(R.id.textView);
         textView.setText(name);
         view.setOnClickListener(view -> Player.Play(context, path));
@@ -25,6 +23,6 @@ public class Item implements Comparable<Item> {
 
     @Override
     public int compareTo(Item item) {
-        return (int) (this.textView.getText().toString().compareTo(item.textView.getText().toString()));
+        return this.textView.getText().toString().compareTo(item.textView.getText().toString());
     }
 }
